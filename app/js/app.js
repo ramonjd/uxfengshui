126288f38748ae391b11742a7078f2cce95893bd
Ramon <ramonjd@gmail.com>
2015-04-05 11:14:49 +0100

    directive shade

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,36 @@
-angular.module('webFengShui',['famous.angular'])
-  .controller('WebFengShuiCtrl', ['$scope', '$famous', function($scope, $famous) {
+angular.module('webFengShui', ['famous.angular'])
+    .controller('WebFengShuiCtrl', ['$scope', '$famous', '$timeout', function ($scope, $famous) {
 
-    var Transitionable = $famous['famous/transitions/Transitionable'];
-    var Easing = $famous['famous/transitions/Easing'];
+        var Transitionable = $famous['famous/transitions/Transitionable'];
+        var Easing = $famous['famous/transitions/Easing'];
 
-    $scope.box = {
-        translate: new Transitionable([0, 150, 0]),
-        opacity: new Transitionable(0)
-    };
+        $scope.box = {
+            translate: new Transitionable([0, 200, 0]),
+            opacity: new Transitionable(0)
+        };
 
-    $scope.head = {
-        translate: new Transitionable([0, 100, 0]),
-        opacity: new Transitionable(0)
-    };
+        $scope.head = {
+            translate: new Transitionable([0, 150, 0]),
+            opacity: new Transitionable(0)
+        };
 
-    $scope.box.translate.set([0, 90, 0], {duration: 400, curve: 'easeInOut'});
-    $scope.box.opacity.set(1, {duration: 1000, curve: 'easeInOut'});
+        $scope.box.translate.set([0, 90, 0], {duration: 700, curve: 'easeInOut'});
+        $scope.box.opacity.set(1, {duration: 1000, curve: 'easeInOut'});
 
-    $scope.head.translate.set([0, 0, 0], {duration: 300, curve: 'easeInOut'});
-    $scope.head.opacity.set(1, {duration: 1000, curve: 'easeInOut'});
+        $scope.head.translate.set([0, 0, 0], {duration: 500, curve: 'easeInOut'});
+        $scope.head.opacity.set(1, {duration: 1000, curve: 'easeInOut'});
 
-}]);
+
+
+
+    }])
+    .directive('shadeBackground', ['$timeout', function ($timeout) {
+        return {
+            restrict : 'E',
+            link : function (scope, element) {
+                $timeout(function () {
+                    element.addClass('fade-out');
+                }, 250);
+            }
+        }
+    }]);
